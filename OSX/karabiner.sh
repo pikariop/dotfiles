6d49e596b4c20f3c8343bb822b302f12a379c2e6
Olli Pikarinen <pikariop@gmail.com>
2015-01-20 17:02:33 +0200

    Configure keyboard settings

---
+++ OSX/karabiner.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
+
+$cli set repeat.wait 63
+/bin/echo -n .
+$cli set repeat.initial_wait 300
+/bin/echo -n .
+$cli set remap.shiftDelete2forwarddelete_nomodifiers 1
+/bin/echo -n .
+/bin/echo
